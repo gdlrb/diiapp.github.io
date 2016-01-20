52861ea007666c27d1b357f802c10b958963a0a8
gdlrb <gdlrb@mail.ru>
2016-01-21 00:54:14 +0300

    create html pages

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 bower install firebase angularfire
 bower install angular-material
+bower install angular-route
 
--- js/appAngular.js
+++ js/appAngular.js
@@ -1,1 +1,39 @@
-var app = angular.module('diApp', ['firebase']);
+var app = angular.module('diApp', [
+    'firebase',
+    'ngRoute',
+    'ngMaterial'
+]);
+
+
+angular.module('diApp')
+    .config(function($routeProvider){
+        $routeProvider
+            .when('/', {
+                controller: 'LocationController',
+                templateUrl: '/pages/home.html'
+            })
+            .when('/login', {
+                controller: 'LocationController',
+                templateUrl: '/login/login.html'
+            })
+            .when('/admin', {
+                controller: 'LocationController',
+                templateUrl: '/admin/admin.html'
+            })
+    })
+    .config(function($locationProvider){
+        $locationProvider.html5Mode({
+            enabled: true
+            /*requireBase: false*/
+        });
+    });
+
+
+angular.module('diApp')
+    .controller('LocationController', function($scope, $location){
+        $scope.location = $location.absUrl();
+    })
+    .controller('AppCtrl', function($scope) {
+
+    });
+
